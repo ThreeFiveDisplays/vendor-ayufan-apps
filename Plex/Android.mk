c3759a908fa497215121eb7a9a238158b0a9c615
ThreeFiveDisplays-Admin <49790926+ThreeFiveDisplays-Admin@users.noreply.github.com>
2021-08-06 14:43:02 -0700

    Delete Plex directory

--- Plex/Android.mk
+++
@@ -1,9 +0,0 @@
-LOCAL_PATH := $(call my-dir)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := Plex
-LOCAL_MODULE_TAGS := optional
-LOCAL_CERTIFICATE := PRESIGNED
-LOCAL_MODULE_CLASS := APPS
-LOCAL_SRC_FILES := com.plexapp.android_6.1.2.812-601281201_minAPI23_apkmirror.com.apk
-include $(BUILD_PREBUILT)
