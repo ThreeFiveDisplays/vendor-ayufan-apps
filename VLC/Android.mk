73bcf9b1ca2d48d202cf1d1f3e2295d839cdfca2
ThreeFiveDisplays-Admin <49790926+ThreeFiveDisplays-Admin@users.noreply.github.com>
2021-08-06 14:43:18 -0700

    Delete VLC directory

--- VLC/Android.mk
+++
@@ -1,9 +0,0 @@
-LOCAL_PATH := $(call my-dir)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := VLC
-LOCAL_MODULE_TAGS := optional
-LOCAL_CERTIFICATE := PRESIGNED
-LOCAL_MODULE_CLASS := APPS
-LOCAL_SRC_FILES := org.videolan.vlc_2.1.12-12011207_minAPI9_apkmirror.com.apk
-include $(BUILD_PREBUILT)
